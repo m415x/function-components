edaff997747d9ad19f3ac84c19da4437c87c16f5
Cristian Lahoz <lahozcristian@gmail.com>
2023-09-13 16:40:32 -0300

    clase02

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import FormSignUp from './components/FormSignUp';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
 
 function App() {
   return (
-    <>
-    <h1>Formulario de Registro</h1>
+    <Container
+      component="article"
+      maxWidth="sm"
+    >
+      <Typography
+        variant='h3'
+        align='center'
+      >Formulario de Registro</Typography>
       <FormSignUp />
-    </>
+    </Container>
   );
 }
 
---
+++ terminal.bat
@@ -0,0 +1,3 @@
+@echo off
+cd "C:\Dev\One\09_function_components\function-components start"
+start cmd.exe /K npm start
